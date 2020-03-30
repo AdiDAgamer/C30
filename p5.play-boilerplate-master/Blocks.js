1ced22b783c292c845e2c847cc0a8b5497e97791
AdiDAgamer <61423116+AdiDAgamer@users.noreply.github.com>
2020-03-30 19:32:07 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/Blocks.js
@@ -0,0 +1,42 @@
+class Block
+{
+    constructor(x, y)
+    {
+        /*var options =
+        {
+            restitution: 1.0
+        }*/
+        this.body = Bodies.rectangle(x, y, 25, 30/*, options*/);
+        this.width = 25;
+        this.height = 30;
+        World.add(world, this.body);
+    }
+
+    display()
+    {
+        if(this.body.speed < 2.5)
+        {
+            var pos = this.body.position;
+            var angle = this.body.angle;
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            fill(0, 255, 0);
+            rect(0, 0, this.width, this.height);
+            pop();
+        }
+
+        else
+        {
+            World.remove(world, this.body);
+            push();
+            this.visibility = this.visibility - 5;
+            tint(255, this.visibility);
+            image(this.image, this.body.position.x, this.body.position.y, 25, 30);
+            pop();
+        }
+
+        
+    }
+}
